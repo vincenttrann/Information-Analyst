2ebf7bfe4d9f24753f0e3120bf8d495c3d3cee25
pchan005 <chanpj@uci.edu>
2019-11-08 23:08:10 -0800

    Update invertedIndex.py

--- invertedIndex.py
+++ invertedIndex.py
@@ -15,13 +15,14 @@ class InvertedIndex:
     def __init__(self):
         self.db = {}
         self.files = []
+        self.doc_id = 1
 
     
     #extracts all the files from a given directory
     def getData(self, path: 'directory path'):
         for root, dirs, files in os.walk(path):
             for name in files:
-                self.files.append(os.path.join(root,name))
+                self.files.append(os.path.join(root, name))
 
 
     #parses json file into url and content tuple
@@ -58,21 +59,25 @@ class InvertedIndex:
     #adds files to database
     def add_to_db(self):
         tempList = []
-        for file in self.files:
-            name =  self.parse_json(file)[0] 
-            content = self.parse_json(file)[1] 
-            text = self.parse_html(content) 
-            tokens = self.stem(self.tokenize(text))
-            for token in sorted(tokens):
-                tempList.append((token, name))
-        for token, name in tempList:
+        with open("doc_id.txt", "w", encoding="utf-8") as f:
+            for file in self.files:
+                doc_id =  self.doc_id 
+                name = self.parse_json(file)[0]
+                content = self.parse_json(file)[1] 
+                text = self.parse_html(content) 
+                tokens = self.stem(self.tokenize(text))
+                f.write(str(doc_id)+" "+str(name)+"\n")
+                for token in sorted(tokens):
+                    tempList.append((token, doc_id))
+                self.doc_id+=1
+        for token, doc_id in tempList:
             if token in self.db.keys():
                 posting = self.db.get(token)[1]
-                if name not in posting:
-                    posting.append(name)
+                if doc_id not in posting:
+                    posting.append(doc_id)
                 self.db[token] = [len(posting), posting]
             else:
-                self.db[token] = [1, [name]]
+                self.db[token] = [1, [doc_id]]
 
 
     def get_db(self):
@@ -89,8 +94,9 @@ if __name__ == '__main__':
     #gets all files in listed path
     i.getData(path)
     i.add_to_db()
-    with open("index.txt", "w", encoding="utf-8") as file:
-        for token, posting in i.get_db().items():
-            file.write(str(token)+": "+str(posting[0])+" -> "+str(posting[1])+"\n")
+    print(i.doc_id)
+    #with open("index.txt", "w", encoding="utf-8") as file:
+        #for token, posting in i.get_db().items():
+            #file.write(str(token)+": "+str(posting[0])+" -> "+str(posting[1])+"\n")
 
     
