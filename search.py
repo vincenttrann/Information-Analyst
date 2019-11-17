3d80715e57018454674a8e41c7a931933b81907e
pchan005 <chanpj@uci.edu>
2019-11-17 13:38:51 -0800

    Create search.py

---
+++ search.py
@@ -0,0 +1,26 @@
+'''
+INF 141 - Assignment 3: Search
+By: Priscilla Chan & Vincent Tran 
+'''
+
+class Search:
+
+    def __init__(self, db):
+        self.db = db 
+
+    
+    def process_query(self, query):
+        pass
+
+
+    def merge(self, p1, p2):
+        pass 
+
+
+    def get_results(self):
+        pass 
+
+
+
+if __name__ == '__main__':
+    pass
