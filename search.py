191b73208806000ad5cddcbe41f8865045828e85
pchan005 <chanpj@uci.edu>
2019-11-17 21:28:11 -0800

    Update search.py

--- search.py
+++ search.py
@@ -2,6 +2,10 @@
 INF 141 - Assignment 3: Search
 By: Priscilla Chan & Vincent Tran 
 '''
+import pickle
+import os
+from index import Posting
+
 
 class Search:
 
@@ -10,17 +14,48 @@ class Search:
 
     
     def process_query(self, query):
-        pass
+        queryList = query.split()
+        if "AND" in queryList:
+            pass 
+        return
 
 
     def merge(self, p1, p2):
-        pass 
+        answer = [] 
+        post = self.longer(p1, p2) 
+        for p in post: 
+            if p1.doc_id == p2.doc_id:
+                answer.append(p1.doc_id) 
+            elif p1.doc_id < p2.doc_id:
+                continue 
+            else:
+                continue 
+        return answer
+
+
+    def longer(self, p1, p2):
+        if p1.length > p2.length:
+            return p1 
+        else:
+            return p2 
 
 
     def get_results(self):
         pass 
 
 
+    def display_results(self):
+        pass
+
+
 
 if __name__ == '__main__':
-    pass
+    while True:
+        query = input('What would you like to search? (Enter q to quit)\n') 
+        if query == 'q':
+            break 
+        else:
+            db = open('index.pkl', 'rb')
+            pickle.load(db)
+            s = Search(db)
+
